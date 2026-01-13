8049c2b2d2d079664e36552f8c9008fb465a6020
Rahmanu Hermawan <rahmanu@pasdirumah.com>
2026-01-13 10:39:17 +0100

    Finish quiz 2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,23 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result = Vec::new();
+        
+        for (string, command) in input {
+            let processed = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => String::from(string.trim()),
+                Command::Append(count) => {
+                    let mut s = string;
+                    s.push_str(&"bar".repeat(count));
+                    s
+                }
+            };
+            result.push(processed);
+        }
+        result
+    }
 }
 
 fn main() {
@@ -39,6 +56,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
