2b728eecdaeda249021673b23cf22275a840d81f
Rahmanu Hermawan <rahmanu@pasdirumah.com>
2026-09-10 13:21:49 +0000

    Finished traits exercise 4 and 5

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -19,7 +19,7 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+fn some_func(item: &(impl SomeTrait + OtherTrait)) -> bool {
     item.some_function() && item.other_function()
 }
 
@@ -33,7 +33,7 @@ mod tests {
 
     #[test]
     fn test_some_func() {
-        assert!(some_func(SomeStruct));
-        assert!(some_func(OtherStruct));
+        assert!(some_func(&SomeStruct));
+        assert!(some_func(&OtherStruct));
     }
 }
